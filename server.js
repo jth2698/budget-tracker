87afd8424012ec1b07a84c76afb4900ded50e0be
James Howard <jth2698@gmail.com>
2020-11-27 17:54:29 -0600

    deploying

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+//dynamic PORT variable
+const PORT = process.env.PORT || 17943;
 
 const app = express();
 
@@ -15,8 +16,11 @@ app.use(express.json());
 
 app.use(express.static("dist"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
