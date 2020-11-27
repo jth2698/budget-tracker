cb7821b055139cf4a88e9ce0adfae48fa69baf86
James Howard <jth2698@gmail.com>
2020-11-26 21:47:01 -0600

    add db.js for indexedDB functionality

--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,11 @@ const FILES_TO_CACHE = [
     '/',
     '/public/index.html',
     '/public/index.js',
+    '/public/db.js',
     '/public/styles.css',
     '/dist/app.bundle.js',
-    'https://fonts.googleapis.com/css?family=Istok+Web|Montserrat:800&display=swap',
-    'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
+    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
 ];
 
 const PRECACHE = 'precache-v1';
