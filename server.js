b3033b97df7981d36c4ea82ed0b4fecf03cb7aba
James Howard <jth2698@gmail.com>
2020-11-27 19:17:07 -0600

    complete refactor using workbox

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("dist"));
+app.use(express.static("public"));
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://localhost/budget", {
---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": "public/",
+  "globPatterns": [
+    "**/*.{js,html,css}"
+  ],
+  "swDest": "public/sw.js"
+};
